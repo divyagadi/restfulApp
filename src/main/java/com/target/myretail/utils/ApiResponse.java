25693f14dcab6f78585b6862d5782937cb9c862a
divya <divya.gadi7@gmail.com>
2021-03-22 17:12:09 +0530

    Committing Code initial commit

---
+++ src/main/java/com/target/myretail/utils/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.target.myretail.utils;
+
+/**
+ * ApiResponse
+ */
+public class ApiResponse {
+
+    private String message;
+
+    public ApiResponse(String message) {
+        this.message = message;
+    }
+}
